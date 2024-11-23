2034a595ad2efa45d5911c504e7a5bebd37362dd
MJ Jacobs <marthinusjacobs@gmail.com>
2024-11-23 15:51:25 +0200

    Created Dimensions.kt
    Took 12 seconds

---
+++ shared/src/commonMain/kotlin/za/co/mjjacobs/noted/ui/Dimensions.kt
@@ -0,0 +1,12 @@
+package za.co.mjjacobs.noted.ui
+
+import androidx.compose.ui.unit.*
+
+/**
+ * Created by MJ Jacobs on 2024/11/23 at 15:41
+ */
+
+val largePadding = 32.dp
+val normalPadding = 16.dp
+val halfPadding = 8.dp
+val smallPadding = 4.dp
